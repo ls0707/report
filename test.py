c5058a33f86050cd8d37305efb03ceeb0c2c222d
ls0707 <ls0707@163.com>
2018-06-18 22:17:28 +0800

    已完成功能： 查找替换 禁止跨页断行

---
+++ test.py
@@ -0,0 +1,33 @@
+from docx import Document
+from docx.shared import Inches
+
+document = Document()
+
+document.add_heading('Document Title', 0)
+
+p = document.add_paragraph('A plain paragraph having some ')
+p.add_run('bold').bold = True
+p.add_run(' and some ')
+p.add_run('italic.').italic = True
+
+document.add_heading('Heading, level 1', level=1)
+document.add_paragraph('Intense quote', style='IntenseQuote')
+
+document.add_paragraph(
+    'first item in unordered list', style='ListBullet'
+)
+document.add_paragraph(
+    'first item in ordered list', style='ListNumber'
+)
+
+
+table = document.add_table(rows=1, cols=3)
+hdr_cells = table.rows[0].cells
+hdr_cells[0].text = 'Qty'
+hdr_cells[1].text = 'Id'
+hdr_cells[2].text = 'Desc'
+
+
+document.add_page_break()
+
+document.save('demo.docx')
