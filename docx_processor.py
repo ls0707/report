534db240558b5c147c0f56e60d1b0af1683b9fed
ls0707 <ls0707@163.com>
2018-07-13 11:16:58 +0800

    自动处理跨页断行、错误关键字替换、4.2章单元测评分析

--- docx_processor.py
+++ docx_processor.py
@@ -12,6 +12,8 @@ from config import Config
 from docx.opc.exceptions import PackageNotFoundError
 from clint.textui import prompt, puts, colored, validators
 import re
+from docx.text.paragraph import Paragraph
+from docx.oxml.xmlchemy import OxmlElement
 
 
 class DocxProcessor(object):
@@ -39,18 +41,148 @@ class DocxProcessor(object):
 
     # ----------------------------- Chapter 4Task
     class ParagraphChapter4Task(Task):
-        def __init__(self, config, doc_position):
+        def __init__(self, config):
             self.cfg = config
-            self.data = list()
-            self.position=doc_position
+
             super(DocxProcessor.ParagraphChapter4Task, self).__init__()
+            self.is_chapter4 = False  # 第四章起始
+            self.is_after_chapter4 = False  # 第四章结束
+            self.load_table_flag = False
+            self.cycle_flag = False
+            self.step_count = -1  # 循环序号
+            self.section_name = ''
+            self.paragraph_ro1 = re.compile('(?<=表)4-\d{1,2}-[12]')
+            self.paragraph_ro2 = re.compile('(结果汇总)|(结果分析)')
+
+            self.table_col_names = list()
+            self.table_satisfy_values = list()
+            self.table_some_and_not_satisfy_values = list()
+            self.unsatisfied_table_id = ''
 
         def __call__(self, paragraph):
-            if self.position.is_chapter4:
-                print(self.position.chapter)
+            if not self.is_after_chapter4:  # 遇到"单元测评小结"（单元测评结束）之前前始终检查每个段落
+                if not self.is_chapter4:
+                    if paragraph.text == '单元测评':
+                        self.is_chapter4 = True
+                else:  # 此处为开始处理文档内容的部分
+                    if paragraph.text not in ('\n', ''):
+                        if paragraph.text == '单元测评小结':
+                            self.is_after_chapter4 = True
+                            self.is_chapter4 = False
+                        else:
+                            # 跳过本章第一段落，由‘物理安全’开始循环每一小节
+                            if paragraph.text == '物理安全':
+                                self.cycle_flag = True
+                            if self.cycle_flag:
+                                # 标记循环序号
+                                r = self.paragraph_ro1.search(paragraph.text)
+                                if r:
+                                    self.step_count += 1
+                                    self.unsatisfied_table_id = r.group()[:-1] + '2'
+                                elif self.step_count == 0 or self.paragraph_ro2.match(paragraph.text):
+                                    self.step_count += 1
+                                else:  # 遇到非预期的段落内容，则认为新小节开始，循环序号置零
+                                    self.step_count = 0
+                                if self.step_count == 0:  # 取出新小节的段落内容作为小节标题
+                                    self.section_name = paragraph.text
+                            # print(str(self.step_count) + paragraph.text)
+                            if paragraph.text == '结果汇总':  # 设定载入标记，下一个表格即将被表格过程调用load_table载入
+                                self.load_table_flag = True
+
+                            elif paragraph.text == '结果分析':  # 设定载入标记，下一个表格即将被表格过程调用load_table载入
+                                ############################
+                                # 生成结果分析文字部分
+                                have_unsatisfied = False
+                                for b in self.table_some_and_not_satisfy_values:
+                                    have_unsatisfied = have_unsatisfied or b
+                                have_satisfied = False
+                                for b in self.table_satisfy_values:
+                                    have_satisfied = have_satisfied or b
+                                # print(have_satisfied, have_unsatisfied)
+                                if have_unsatisfied:
+                                    # 如果有不符合项时，则文档内有不符合项表4-x-2
+                                    satisfied_list = list(filter(lambda x: x != '',
+                                                                 map(lambda x, y, z: x if y and not z else '',
+                                                                     self.table_col_names,
+                                                                     self.table_satisfy_values,
+                                                                     self.table_some_and_not_satisfy_values)))
+                                    unsatisfied_list = list(filter(lambda x: x != '',
+                                                                   map(lambda x, y: x if y else '',
+                                                                       self.table_col_names,
+                                                                       self.table_some_and_not_satisfy_values)))
+                                    # print(self.unsatisfied_table_id)
+                                    # print(self.section_name, satisfied_list, unsatisfied_list)
+                                    if len(satisfied_list) > 0:
+                                        output_str1 = '符合项分析：物理安全除表{}所列项目之外，在{}方面均符合要求，在{}方面具备一定的安全性。'.format(
+                                            self.unsatisfied_table_id, '、'.join(satisfied_list) + (
+                                                '{}'.format('等' if len(satisfied_list) > 1 else '')), self.section_name)
+                                    else:
+                                        output_str1 = '应用安全在各方面均存在一些问题。'
+                                    output_str2 = '部分符合和不符合项分析：{}在{}方面还存在一些问题，详见表{}。'.format(self.section_name,
+                                                                                             '、'.join(
+                                                                                                 unsatisfied_list) + (
+                                                                                                 '{}'.format('等' if len(
+                                                                                                     unsatisfied_list) > 1 else '')),
+                                                                                             self.unsatisfied_table_id)
+                                    print(output_str1)
+                                    print(output_str2)
+                                    opp = self.insert_paragraph_after(paragraph, output_str1, 'NER-CONTENTTEXT3')
+                                    self.insert_paragraph_after(opp, output_str2, 'NER-CONTENTTEXT3')
+                                elif not have_unsatisfied and have_satisfied:
+                                    # 如果没有不符合项时，则文档内无不符合项表
+                                    satisfied_list = self.table_col_names
+                                    output_str = '在{}方面均符合要求，在{}方面具备一定的安全性。'.format('、'.join(self.table_col_names) + (
+                                        '{}'.format('等' if len(satisfied_list) > 1 else '')),
+                                                                                    self.section_name)
+                                    print(output_str)
+                                    self.insert_paragraph_after(paragraph, output_str, 'NER-CONTENTTEXT3')
+                                else:
+                                    pass
+                                    print('-' * 20)
+                                ############################
+
+        def insert_paragraph_after(self, paragraph, text=None, style=None):
+            """Insert a new paragraph after the given paragraph."""
+            new_p = OxmlElement("w:p")
+            paragraph._p.addnext(new_p)
+            new_para = Paragraph(new_p, paragraph._parent)
+            if text:
+                new_para.add_run(text)
+            if style is not None:
+                new_para.style = style
+            return new_para
 
         def load_table(self, table):
-            self.data.clear()
+            self.table_satisfy_values = [False for x in range(15)]
+            self.table_some_and_not_satisfy_values = [False for x in range(15)]
+            if self.load_table_flag:
+                # 保存“结果汇总表”的数据
+                for i, row in enumerate(table.rows):
+                    try:
+                        if i == 1:
+                            self.table_col_names = [cell.text for cell in row.cells[3:]]
+                        elif (i - 2) % 4 == 0:
+                            if len(self.table_satisfy_values) > 0:
+                                self.table_satisfy_values = list(map(lambda x, y: x or y, self.table_satisfy_values,
+                                                                     [bool(int(cell.text)) if i > 2 else
+                                                                      cell.text for i, cell in
+                                                                      enumerate(row.cells)][3:]))
+
+                        elif i > 1 and ((i - 2) % 4 == 1 or (i - 2) % 4 == 2):
+                            if len(self.table_some_and_not_satisfy_values) > 0:
+                                self.table_some_and_not_satisfy_values = list(map(lambda x, y: x or y,
+                                                                                  self.table_some_and_not_satisfy_values
+                                                                                  , [bool(int(cell.text)) if i > 2 else
+                                                                                     cell.text for i, cell in
+                                                                                     enumerate(row.cells)][3:]))
+                        else:
+                            pass
+                    except Exception as e:
+                        print(i, e)
+                # print(self.table_col_names)
+                # print(self.table_satisfy_values)
+                # print(self.table_some_and_not_satisfy_values)
+            self.load_table_flag = False  # 重置标记，等待段落过程重新调用__call__中的设置标志
 
     # ----------------------------- Cant split Task
     class TableCantsplitTask(Task):
@@ -59,14 +191,13 @@ class DocxProcessor(object):
 
         def __call__(self, table):
             for row in table.rows:
-                print(row)
                 tp = parse_xml(r'<w:cantSplit {}/>'.format(nsdecls('w')))
                 if row._tr.trPr is None:  # trPr为None的情况直接添加属性
-                    print(tp, '--added')
+                    # print(tp, '--added')
                     row._tr.get_or_add_trPr().append(tp)
                 else:  # trPr不为None的情况下才做属性遍历，判断该属性是否已经存在
                     for p in row._tr.trPr.getchildren():
-                        print(p, '--discovered')
+                        # print(p, '--discovered')
                         if 'cantSplit' not in p.tag:
                             row._tr.get_or_add_trPr().append(tp)
 
@@ -77,30 +208,6 @@ class DocxProcessor(object):
 
         def __call__(self, table):
             pass
-
-    # ----------------------------- Position
-    class DocPosition(object):
-        def __init__(self):
-            self.__ro_chapter = re.compile('\d+(\.\d+)+')
-            self.__chapter = list()
-            self.this_chapter = list()
-
-        def location_from_paragraph(self, paragraph):
-            ma_chapter = self.__ro_chapter.match(paragraph.text)
-
-            if ma_chapter:
-                self.__chapter = ma_chapter.group().split('.')
-
-        @property
-        def chapter(self):
-            return self.__chapter
-
-        def is_chapter4(self):
-            result = False
-            if len(self.__chapter) > 0:
-                if self.__chapter[0] == 4:
-                    result = True
-            return result
 
     # ============================= DocxProcessor
     def __init__(self, processor_cfg, docx_file):
@@ -113,9 +220,9 @@ class DocxProcessor(object):
         except PackageNotFoundError as e:
             print(e)
             exit(0)
-        self.position=DocxProcessor.DocPosition()
+
         self.paragraph_replace_task = DocxProcessor.ParagraphReplaceTask(processor_cfg)
-        self.paragraph_chapter4_task = DocxProcessor.ParagraphChapter4Task(processor_cfg, self.position)
+        self.paragraph_chapter4_task = DocxProcessor.ParagraphChapter4Task(processor_cfg)
         self.table_cantsplit_task = DocxProcessor.TableCantsplitTask()
         self.table_chapter6_task = DocxProcessor.TableChapter6Task()
 
@@ -157,7 +264,7 @@ class DocxProcessor(object):
                     exit(0)
 
     def process_paragraph(self, paragraph):
-        self.position.location_from_paragraph(paragraph)
+
         for func in self.paragraph_process_list:  # 顺次以paragraph为参数，执行列表中的函数
             if func is not None:
                 func(paragraph)
--- run.py
+++ run.py
@@ -4,5 +4,5 @@ from config import Config
 from docx_processor import DocxProcessor
 
 cfg=Config('config.ini')
-p=DocxProcessor(cfg, 'e:/1/3.docx')
+p=DocxProcessor(cfg, 'd:/1/3.docx')
 p.start()
